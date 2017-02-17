f6d22f3e38880dc321528f16ec221105097798e0
cyptertrais <cyprien.tertrais@hotmail.com>
2017-02-17 07:25:08 +0100

    v2

--- tppoo/src/tp4/EssaiAgenda.java
+++ tppoo/src/tp4/EssaiAgenda.java
@@ -4,11 +4,12 @@ public class EssaiAgenda {
 
 	public static void main(String[] args) {
 		Agenda agenda= new Agenda();
-		Evenement e1= new Evenement("Docteur","Paris",LocalDate.of(2017, 02, 19),LocalDate.of(2017, 03, 27));
+		Evenement e1= new Evenement("Docteur","Paris",LocalDate.of(2017, 02, 18),LocalDate.of(2017, 03, 27));
 		Evenement e2= new Evenement("Donner une piece a Alexis","IUT",LocalDate.of(2017, 02, 16),LocalDate.of(2017, 02, 17));
 		agenda.entrerEvenement(e1);
-		agenda.entrable(e2);
-		System.out.println(e1.chevauche(e2));
+		agenda.entrerEvenement(e2);
+		agenda.supprimerEvenement(0);
+		System.out.println(agenda);
 	}
 
 }
